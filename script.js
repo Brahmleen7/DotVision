8cf520610f00757067a50a7559be32c272625ec7
Brahmleen Kaur <brahmleen@gmail.com>
2025-03-18 17:23:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
+    alert("Neural Queens welcomes you to DotVision!");
+    
     const navLinks = document.getElementById("navLinks");
     const toggleButton = document.querySelector(".menu-toggle");
 
